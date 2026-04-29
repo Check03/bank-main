a1e960d13cf4cf6df488285e97633d928ad5393d
Check <ivankulikov3333@gmail.com>
2026-04-30 01:55:19 +0300

    modified:   bank-app/src/App.js

--- bank-app/src/App.js
+++ bank-app/src/App.js
@@ -9,6 +9,7 @@ import Transfer from "./components/Transfer";
 import Contacts from "./components/Contacts";
 import Admin from "./components/Admin";  
 import Profile from "./components/Profile";
+import Profile from "./components/Accounts";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />  
           <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route path="/accounts" element={<PrivateRoute><Accounts /></PrivateRoute>} />
         </Routes>
       </AuthProvider>
     </Router>
