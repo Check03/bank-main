87dddd2034719ba28951df0450e8ab93a0bb8984
Check <ivankulikov3333@gmail.com>
2026-04-29 17:55:43 +0300

    modified:   bank-app/src/components/Dashboard.js

--- bank-app/src/components/Dashboard.js
+++ bank-app/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { db } from "../firebase";
-import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 const currencySymbols = {
   RUB: "₽",
